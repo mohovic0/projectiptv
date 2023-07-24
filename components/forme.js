35d6350cfd4be230ae1c5e3264f56ecadf14e046
mohovic0 <108498992+mohovic0@users.noreply.github.com>
2023-07-24 22:38:26 +0100

    f

--- components/forme.js
+++ components/forme.js
@@ -94,7 +94,7 @@ function Forme({pack}) {
                           <div className="mb-3">
                             <label htmlFor="exampleInputEmail1" className="form-label">Email address *</label>
                             <input required  value={email} onChange={(e)=>setEmail(e.target.value)} type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-                            <div id="emailHelp" className="form-text">We ll never share your email with anyone else. {email}</div>
+                            <div id="emailHelp" className="form-text">We ll never share your email with anyone else.</div>
                           </div>
 
                           <div className="mb-3">
