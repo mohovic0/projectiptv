a113b21589345faed2145c65d39222e840619e04
mohovic0 <108498992+mohovic0@users.noreply.github.com>
2023-08-03 13:55:08 +0100

    update metadata

--- pages/contact/pack3.js
+++ pages/contact/pack3.js
@@ -17,22 +17,34 @@ function Pack3() {
     return (
       <>  
         <Head>
-          <title>ipTv Infinity - Best IPTV Subscription Service Provider</title>
-          <meta name="description" content="ipTv Infinity is the best on the planet, 
-                we always strive hard for perfection and your satisfaction. We give you
-                 the stability and reliability of the service. Get the next GEN+ IPTV." />
-          <link rel="icon" type="image/png" href="/logo.png" sizes="32x32" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <meta property="og:locale" content="en_US"/>
-          <meta property="og:type" content="website"/>
-          <meta property="og:title" content="ipTv Infinity - Best IPTV Subscription Service Provider"/>
-          <meta property="og:description" content="ipTv Infinity is the best on the planet, 
-                we always strive hard for perfection and your satisfaction. We give you
-                 the stability and reliability of the service. Get the next GEN+ IPTV."/>
-          <meta property="og:url" content="https://"/>
-          <meta property="og:site_name" content="IPTV-Infinity"/>
-          <meta property="og:image" content="https://"/>
-        </Head>
+            <title>ipTv Infinity - Best IPTV Subscription Service Provider</title>
+            <meta name="description" content="ipTv Infinity is the best on the planet, 
+                  we always strive hard for perfection and your satisfaction. We give you
+                  the stability and reliability of the service. Get the next GEN+ IPTV." />
+            <link rel="icon" type="image/png" href="/logo.png" sizes="32x32" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <meta name="generator" content="Next.js" />
+            <meta property="og:locale" content="en_US"/>
+            <meta property="og:type" content="website"/>
+            <meta property="og:title" content="ipTv Infinity - Best IPTV Subscription Service Provider"/>
+            <meta property="og:description" content="ipTv Infinity is the best on the planet, 
+                  we always strive hard for perfection and your satisfaction. We give you
+                  the stability and reliability of the service. Get the next GEN+ IPTV."/>
+            <meta name="keywords" content="iptv subscription, iptv subscription legal, best iptv subscription, iptv subscription reddit, 
+                  ipTv Infinity subscription, iptv smarters pro subscription, iptv smarters subscription, iptv subscription ebay,
+                  ip tv subscription, iptv subscribe, iptv sub, iptv smarters subscription reddit, iptvsubscription tv, iptv subscription service,
+                  iptv 12 months, iptv lifetime subscription, iptv subscription provider, iptv subscription so player, best iptv subscription for firestick,
+                  buy iptv subscription, mag subscription, best iptv subscription 2023 reddit, best iptv subscription for firestick reddit,
+                  iptv subscription dubai, iptv subscription canada reddit, iptv smarters subscription aliexpress, iptv subscription saudi arabia,
+                  iptv subscription for apple tv 4k, iptv box and subscription, iptv box monthly, iptv box price, iptv subscription uk reddit,
+                  iptv subscription uk ebay, reddit best iptv subscription, smart iptv subscription ebay,smart iptv subscription reddit,
+                  mag 322 w1 iptv box subscription, best arabic iptv 2023 subscription services, 
+                  " />
+            <meta property="og:url" content="https://www.iptvinfinity.tech"/>
+            <meta property="og:site_name" content="IPTV-Infinity"/>
+            <meta property="og:image" content="https://github.com/auzomaki/imagesogiptvinfinity/blob/main/ogimage.PNG"/>
+      </Head>
+
   
         <div className={`${styles.menbackgound}`}></div> 
         <section>
