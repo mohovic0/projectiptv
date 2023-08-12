f03d6b62e904324737060d6fa76789ede233390d
mohovic0 <108498992+mohovic0@users.noreply.github.com>
2023-08-12 21:16:11 +0100

    up date home

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ import Carousel  from '@/components/carousel'
 import Link from 'next/link'
 import Image from 'next/image'
 import { useRef } from 'react'
-import imgsub from '@/public/iptvarticle.jpg'
+import styled from '@/pages/faq/Faq.module.css'
 
 
 export default function Home() {
@@ -136,48 +136,298 @@ export default function Home() {
           </div>
       </section>
 
-      <section className='container pt-5' >
-            <Image
-                    className='container'
-                    src={imgsub}
-                    alt="image subscraption"
+      <section className={`container-fluid pt-5 pb-2 mb-3 ${styled.section1}`}>
+              <h1 className="fs-1 fw-bolder text-black pt-5 mt-3 text-center mb-4">FAQ</h1>
+              <div className="accordion accordion-flush" id="accordionFlushExample">
+
+                <div className={`accordion-item ${styled.accordionBtn}`}>
+                  <h2 className="accordion-header">
+                    <button
+                      className="accordion-button collapsed"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target="#flush-collapseOne"
+                      aria-expanded="false"
+                      aria-controls="flush-collapseOne"
+                    >
+                      <strong>HOW DOES IT WORK?</strong>
+                    </button>
+                  </h2>
+                  <div
+                    id="flush-collapseOne"
+                    className="accordion-collapse collapse"
+                    data-bs-parent="#accordionFlushExample"
+                  >
+                    <div className="accordion-body">
+                      To watch our TV channels FASTIGERIPTV, you must first have a
+                      stable Internet connection, as well as a Smart device. The Smart
+                      Devices and Applications we support and you can watch TV channels
+                      from
+                          <ul className="mt-4 ">
+                            <li className="mt-2">
+                              Smart TV (Samsung, LG, Philips, Panasonic, Grundig)
+                            </li>
+                            <li className="mt-2">Android Box</li>
+                            <li className="mt-2">
+                              Android Devices (Including All Devices that they have OS of
+                              Android)
+                            </li>
+                            <li className="mt-2">Mag Boxes (250, 254, etc)</li>
+                            <li className="mt-2">Dreamlink (T1, T1+, etc)</li>
+                            <li className="mt-2">GSE Smart IPTV Pro</li>
+                            <li className="mt-2">Avov Tvonline (N, N2, N3 etc)</li>
+                            <li className="mt-2">Enigma</li>
+                            <li className="mt-2">STB Emulator</li>
+                            <li className="mt-2">iOS Devices (iPhone, iPad)</li>
+                            <li className="mt-2">SS-IPTV</li>
+                          </ul>
+                    </div>
+                  </div>
+                </div>
+
+
+                <div className={`accordion-item ${styled.accordionBtn}`}>
+                  <h2 className="accordion-header">
+                    <button
+                      className="accordion-button collapsed"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target="#flush-collapseTwo"
+                      aria-expanded="false"
+                      aria-controls="flush-collapseTwo"
+                    >
+                      <strong>WHEN WILL I RECEIVE MY TEST/SUBSCRIPTION CREDENTIALS?</strong>
+                    </button>
+                  </h2>
+                  <div
+                    id="flush-collapseTwo"
+                    className="accordion-collapse collapse"
+                    data-bs-parent="#accordionFlushExample"
+                  >
+                    <div className="accordion-body">
+                    You will get your file as soon as possible. We will do our best not to let you wait long.
+                    Your test / subscription will be in your Email as fast as possible.
+                    </div>
+                  </div>
+                </div>
+
+
+                <div className={`accordion-item ${styled.accordionBtn}`}>
+                  <h2 className="accordion-header">
+                    <button
+                      className="accordion-button collapsed"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target="#flush-collapseThree"
+                      aria-expanded="false"
+                      aria-controls="flush-collapseThree"
+                    >
+                      <strong>DOES THIS SERVICE (IPTV) WORK IN MY COUNTRY?</strong>
+                    </button>
+                  </h2>
+                  <div
+                    id="flush-collapseThree"
+                    className="accordion-collapse collapse"
+                    data-bs-parent="#accordionFlushExample"
+                  >
+                    <div className="accordion-body">
+                        IPTV works all over the world, no matter where you live or where you are.
+                        Since you have a stable Internet connection of 8 MB / s, you will be able
+                        to watch thousands of TV channels.
+                    </div>
+                  </div>
+                </div>
+
+                <div className={`accordion-item ${styled.accordionBtn}`}>
+                  <h2 className="accordion-header">
+                    <button
+                      className="accordion-button collapsed"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target="#flush-collapseFour"
+                      aria-expanded="false"
+                      aria-controls="flush-collapseFour"
+                    >
+                      <strong>HOW MUCH INTERNET SPEED REQUIRES TO WATCH IPTV CHANNELS?</strong>
+                    </button>
+                  </h2>
+                  <div
+                    id="flush-collapseFour"
+                    className="accordion-collapse collapse"
+                    data-bs-parent="#accordionFlushExample"
+                  >
+                    <div className="accordion-body">
+                    To watch our IPTV channels, you must have a stable 6 MB / s internet for SD channels, 8 MB / s for HD channels and stable 10 MB / s for FHD channels. To test your internet faster, you do not have to test it through Wi-Fi, but directly through the LAN cable, placing it on your Android Box, or on your computer. Note: Your Internet should stay stable,
+                    if your Internet browser drops down, then you may also have problems with TV channels.
+                    </div>
+                  </div>
+                </div>
+
+
+                <div className={`accordion-item ${styled.accordionBtn}`}>
+                  <h2 className="accordion-header">
+                    <button
+                      className="accordion-button collapsed"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target="#flush-collapseFive"
+                      aria-expanded="false"
+                      aria-controls="flush-collapseFive"
+                    >
+                      <strong>AFTER SUBSCRIPTION, WHAT SHOULD I DO NEXT?</strong>
+                    </button>
+                  </h2>
+                  <div
+                    id="flush-collapseFive"
+                    className="accordion-collapse collapse"
+                    data-bs-parent="#accordionFlushExample"
+                  >
+                    <div className="accordion-body">
+                    Immediately after you place an order, you will automatically receive an email
+                    containing all the necessary instructions, and after a short time you will also receive 
+                    your subscription data. However sometimes we get overloaded with server support, customers and orders.
+                    </div>
+                  </div>
+                </div>
+
+
+                <div className={`accordion-item ${styled.accordionBtn}`}>
+                  <h2 className="accordion-header">
+                    <button
+                      className="accordion-button collapsed"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target="#flush-collapseSix"
+                      aria-expanded="false"
+                      aria-controls="flush-collapseSix"
+                    >
+                      <strong>CAN I PAY THROUGH MY LOCAL CURRENCY?</strong>
+                    </button>
+                  </h2>
+                  <div
+                    id="flush-collapseSix"
+                    className="accordion-collapse collapse"
+                    data-bs-parent="#accordionFlushExample"
+                  >
+                    <div className="accordion-body">
+                        Please Note that our prices are quoted in the European Euro (EUR €) and United State Dollar ( USD $).
+                    </div>
+                  </div>
+                </div>
+
+
+                <div className={`accordion-item ${styled.accordionBtn}`}>
+                  <h2 className="accordion-header">
+                    <button
+                      className="accordion-button collapsed"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target="#flush-collapseSeven"
+                      aria-expanded="false"
+                      aria-controls="flush-collapseSeven"
+                    >
+                      <strong>HOW DO YOU DELIVER MY SUBSCRIPTION?</strong>
+                    </button>
+                  </h2>
+                  <div
+                    id="flush-collapseSeven"
+                    className="accordion-collapse collapse"
+                    data-bs-parent="#accordionFlushExample"
+                  >
+                    <div className="accordion-body">
+                      We will send you your subscription via Email, as well as any Guidelines for any device you have.
+                    </div>
+                  </div>
+                </div>
+
+
+                <div className={`accordion-item ${styled.accordionBtn}`}>
+                  <h2 className="accordion-header">
+                    <button
+                      className="accordion-button collapsed"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target="#flush-collapseEight"
+                      aria-expanded="false"
+                      aria-controls="flush-collapseEight"
+                    >
+                      <strong>WHAT PAYMENT METHODS DO YOU ACCEPT?</strong>
+                    </button>
+                  </h2>
+                  <div
+                    id="flush-collapseEight"
+                    className="accordion-collapse collapse"
+                    data-bs-parent="#accordionFlushExample"
+                  >
+                    <div className="accordion-body">
+                      We support 2 payment methods at the moment, with a high security.
+                      The payment methods we support are: <strong>PAYPAL - IBAN </strong>
+                    </div>
+                  </div>
+                </div>
+
+
+                <div className={`accordion-item ${styled.accordionBtn}`}>
+                  <h2 className="accordion-header">
+                    <button
+                      className="accordion-button collapsed"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target="#flush-collapseNine"
+                      aria-expanded="false"
+                      aria-controls="flush-collapseNine"
+                    >
+                      <strong>IS MY PAYMENT SAFE?</strong>
+                    </button>
+                  </h2>
+                  <div
+                    id="flush-collapseNine"
+                    className="accordion-collapse collapse"
+                    data-bs-parent="#accordionFlushExample"
+                  >
+                    <div className="accordion-body">
+                        You can Pay securely via Credit Card without exposing your details to any one. When paying online, your details are protected by SSL Encryption.
+                    </div>
+                  </div>
+                </div>
+
+
+
+                <div className={`accordion-item ${styled.accordionBtn}`}>
+                  <h2 className="accordion-header">
+                    <button
+                      className="accordion-button collapsed"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target="#flush-collapseTen"
+                      aria-expanded="false"
+                      aria-controls="flush-collapseTen"
+                    >
+                      <strong>WHY ARE MY CHANNELS NOT WORKING?</strong>
+                    </button>
+                  </h2>
+                  <div
+                    id="flush-collapseTen"
+                    className="accordion-collapse collapse"
+                    data-bs-parent="#accordionFlushExample"
+                  >
+                    <div className="accordion-body">
+                    We do the best for all channels to work smoothly and all right.
+                    If only some channels do not work, then report it to us by contacting us, or wait a few hours, maybe we’re working to fix it.
+                    If no channel is running, then you need to take some necessary steps before contacting us:
                     
-            />
-            <h1 className='text-center'>Unleashing the Ultimate Entertainment Experience: Exploring <strong>IPTV subscription</strong> Services</h1>
-            <h2>Introduction</h2>
-            <p>In the dynamic landscape of modern entertainment, Internet Protocol Television (IPTV) subscription services have emerged as a groundbreaking solution, redefining the way we consume television content. With an impressive range of channels, on-demand programs, and interactive features, <strong>IPTV subscriptions</strong> have swiftly become a cornerstone of contemporary leisure. This article delves into the captivating realm of <strong>IPTV subscription</strong> services, shedding light on their benefits, features, and the profound impact they ve had on transforming our TV-watching habits.</p>
-            <h2>Evolution of Television with <strong>IPTV subscriptions</strong></h2>
-            <p><strong>IPTV subscription</strong> services represent a paradigm shift in how we access and enjoy television. Departing from conventional cable and satellite broadcasting, IPTV utilizes internet protocols to deliver captivating audiovisual content directly to viewers screens. This innovation empowers users to indulge in their preferred shows, movies, and live events through an array of devices, including smart TVs, smartphones, tablets, and computers.</p>
-            <h2>Key Elements of <strong>IPTV subscription</strong> Services</h2>
-            <p>a) Diverse Content Selection: IPTV subscription services boast an impressive array of channels spanning genres like news, sports, entertainment, lifestyle, and more. This rich diversity caters to a wide spectrum of preferences, ensuring every viewer finds their desired content.
+                    <ul className="mt-2">
+                      <li className="mt-2"> Reboot your devices and wait for 30 secs before turning them back on.</li>
+                      <li className="mt-2">Make sure you are connecting using an Ethernet cable to devices and not via Wi-Fi.</li>
+                      <li className="mt-2">Try using a VPN service if all the above are not working.</li>
+                      <li className="mt-2">Stop and give it a couple of minutes. We reboot and swap channels when needed. The channels may be coming back from our work.</li>
+                    </ul>
+                  
+                    </div>
+                  </div>
+                </div>
 
-            b) On-Demand Delights: A standout feature of <strong>IPTV subscriptions</strong> is the provision of video-on-demand (VOD) content. With an extensive library of movies, TV series, and special programs, users can curate their own viewing schedules.
-
-            c) Engagement Redefined: IPTV brings an element of interactivity to the TV-watching experience. Users can participate in real-time polls, interactive quizzes, and engage in live chats related to their favorite shows, fostering a sense of community.
-
-            d) Seamless Multi-Device Access: The flexibility of <strong>IPTV subscriptions</strong> shines as users effortlessly switch between devices. Whether at home or on the go, viewers can pick up where they left off, ensuring an uninterrupted entertainment journey.
-            </p>
-            <h2>Navigating the <strong>IPTV subscription</strong> Landscape</h2>
-            <p>
-            When selecting an <strong>IPTV subscription</strong> service, making an informed choice can significantly enhance your entertainment venture. Consider these factors to ensure a satisfying and dependable experience:
-
-            a) Content Versatility: Evaluate the range and quality of available channels and VOD options, aligning them with your interests and preferences.
-
-            b) Streaming Quality: Prioritize services that offer high-definition (HD) and 4K streaming to elevate your viewing experience to new heights.
-
-            c) Device Compatibility: Confirm that your preferred devices, such as smart TVs, smartphones, tablets, and streaming gadgets, are supported by the chosen service.
-
-            d) Reliability and Consistency: Research the service s reputation for stability and uptime to avoid interruptions during your favorite shows or live events.
-            
-            e) Responsive Customer Support: Opt for services that provide responsive customer support to promptly address any technical queries or concerns.
-            </p>
-
-            <h2>Conclusion</h2>
-            
-            <p>
-            <strong>IPTV subscription</strong> services have ushered in an era of entertainment that marries convenience, flexibility, and interactive features. With an extensive selection of channels, on-demand content, and the freedom to view across multiple devices, <strong>IPTV subscriptions</strong> seamlessly cater to the evolving preferences of today s viewers. For <strong>IPTV subscription</strong> providers,  attract a broader audience, and solidify a strong position in the competitive entertainment domain. As technology continues to evolve, <strong>IPTV subscriptions</strong> are poised to play a pivotal role in shaping the future of television consumption, enriching our entertainment experiences beyond imagination.
-            </p>
-
+              </div>
       </section>
 
          
