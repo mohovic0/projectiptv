7c0f3a1d311fa714d2e4007cfa18238e90373310
mohovic0 <108498992+mohovic0@users.noreply.github.com>
2023-08-13 16:32:42 +0100

    add canonical url

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -39,6 +39,7 @@ function Contact() {
         <meta property="og:url" content="https://www.iptvinfinity.tech"/>
         <meta property="og:site_name" content="IPTV-Infinity"/>
         <meta property="og:image" content="https://raw.githubusercontent.com/auzomaki/imagesogiptvinfinity/main/ogimage.PNG"/>
+        <link rel="canonical" href="https://www.iptvinfinity.tech/contact" />
       </Head>
 
 
