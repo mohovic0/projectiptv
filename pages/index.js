7b96377c67b9efe97fee05a8e9befa8e2f45077c
mohovic0 <108498992+mohovic0@users.noreply.github.com>
2023-09-01 00:46:12 +0100

    changing the price and size of card

--- pages/index.js
+++ pages/index.js
@@ -30,6 +30,28 @@ export default function Home() {
   return (
     <>  
       <Head>
+
+              
+      
+      <script>
+        {`
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '777901347679516');
+        fbq('track', 'PageView');
+        `}
+      </script>
+      <noscript><Image alt={"fb"} height={1} width={1} style={{display: "none"}}
+        src={`https://www.facebook.com/tr?id=777901347679516&ev=PageView&noscript=1`}
+      /></noscript>
+
+
         
         <title>The Best IPTV Subscription Service Provider-UK-Canada-France ...</title>
         <meta name="description" content="UK-Canada-France...-Infinity IPTV is the best on the planet, 
