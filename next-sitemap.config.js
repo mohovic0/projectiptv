a42a6988c6a6c8f2f8307ffd384ffb3696213e69
mohovic0 <108498992+mohovic0@users.noreply.github.com>
2023-11-03 06:51:31 +0100

    optim5

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: "https://www.iptv-streaming.me",
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+  } 
